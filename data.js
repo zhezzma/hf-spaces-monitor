9eb48bc846f5b978fd03f6d2847a69b91166be6d
zhezzma <zhezzma@users.noreply.github.com>
2025-06-14 08:16:12 +0000

    deploy: 90d29e3e428f42d4a5bec823efca9ae72a73b966

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
 const spaceStatusData = {
-  "2025-06-14 12:17:28": {
+  "2025-06-14 16:16:11": {
     "cloudstudio-runner-zheooma": {
       "status": true,
-      "duration": "0.11秒"
+      "duration": "0.42秒"
     },
     "cloudstudio-runner": {
       "status": true,
-      "duration": "0.14秒"
+      "duration": "0.40秒"
     },
     "cnb-runner-godgodgame": {
       "status": true,
-      "duration": "0.15秒"
+      "duration": "0.30秒"
     },
     "cnb-runner-igiven": {
       "status": true,
-      "duration": "0.30秒"
+      "duration": "0.14秒"
     },
     "cnb-runner-zhepama": {
       "status": true,
-      "duration": "0.22秒"
+      "duration": "0.12秒"
     },
     "cursor2api": {
       "status": true,
-      "duration": "0.10秒"
+      "duration": "0.14秒"
     },
     "msmail": {
       "status": true,
-      "duration": "0.09秒"
+      "duration": "0.11秒"
     },
     "trae2api": {
       "status": true,
-      "duration": "0.09秒"
+      "duration": "0.11秒"
     }
   }
 };
