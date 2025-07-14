58818e6d04b91f4997127dc29d2bf02b6f16f15e
zhezzma <zhezzma@users.noreply.github.com>
2025-07-14 12:18:00 +0000

    deploy: 6fd65110cd6338c5070ef9bacba46fbf46c2c42e

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 const spaceStatusData = {
-  "2025-07-14 16:14:43": {
+  "2025-07-14 20:17:59": {
     "tencent-ide-runner": {
-      "status": true,
-      "duration": "0.23秒"
-    },
-    "cloudstudio-runner": {
       "status": true,
       "duration": "0.15秒"
     },
+    "cloudstudio-runner": {
+      "status": true,
+      "duration": "0.21秒"
+    },
     "cnb-runner": {
       "status": true,
-      "duration": "0.14秒"
+      "duration": "0.19秒"
     },
     "msmail": {
       "status": true,
-      "duration": "0.17秒"
+      "duration": "0.12秒"
     }
   }
 };
